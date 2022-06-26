ca83498b8c32633a7ab096d2af262d95a24df913
ThiagodaSilva27 <thiagotwoseven@gmail.com>
2022-06-26 15:41:45 -0300

    fix: substituirNumerosPares

---
+++ Javascript/SubstituirNumeroPares/scripts.js
@@ -0,0 +1,19 @@
+const substituirNumerosPares = (arrayNumeros) => {
+    const novoArray = [];
+    if (arrayNumeros != '') {
+        for (let i = 0; i < arrayNumeros.length; i++) {
+            const numero = Number(arrayNumeros[i]);
+            if ((numero % 2) == 0 && numero !== 0) {
+                novoArray.push(0)
+            } else {
+                novoArray.push(numero)
+            }
+        }
+        alert(novoArray)
+    } else {
+        alert('-1')
+    }
+};
+const arrayNumeros = [1, 3, 4, 6, 80, 33, 23, 90];
+const arrayVazio = []
+substituirNumerosPares(arrayNumeros);
